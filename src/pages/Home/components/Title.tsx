7fa875eba0d37fbbe623b4cdf6450ac30aa69715
victor <robin_fc@qq.com>
2023-09-27 10:55:45 +0800

    perf: 字符子集化

--- src/pages/Home/components/Title.tsx
+++ src/pages/Home/components/Title.tsx
@@ -4,14 +4,15 @@ import { useControls } from 'leva'
 import { useLayoutEffect, useMemo, useRef } from 'react'
 import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry'
 import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js'
+import { Color } from 'three'
 
 extend({ TextGeometry })
 
 const Title = () => {
   const ref: any = useRef()
-  const { color, text } = useControls({ color: 'aqua', text: 'FL426星球正在建设中，你可以先在我的地球工作室随便逛逛' })
-  const font = useLoader(FontLoader, '/typefaces/Ma Shan Zheng_Regular.json')
-  const config = useMemo(() => ({ font, size: 0.1, height: 0 }), [font])
+  const { color, text } = useControls({ color: 'white', text: 'FL426星球正在建设中，你可以先在我的地球工作室随便逛逛' })
+  const font = useLoader(FontLoader, '/typefaces/FZHei-B01S_Regular.json')
+  const config = useMemo(() => ({ font, size: 0.1, height: 0.04 }), [font])
 
   const { camera } = useThree()
 
@@ -32,7 +33,7 @@ const Title = () => {
   return (
     <mesh ref={ref} position={[0, 2.2, 0]} rotation={[0, Math.PI / 4, 0]}>
       <textGeometry args={[text, config]} />
-      <meshStandardMaterial color={color} />
+      <meshStandardMaterial color={color} roughness={0.5}/>
     </mesh>
   )
 }
