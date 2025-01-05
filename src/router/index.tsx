1a73f9ac654397fb46079caba0a79dbbf92e2f8c
victor <873602646@qq.com>
2025-01-05 10:11:46 +0800

    chore: delete old design

--- src/router/index.tsx
+++
@@ -1,9 +0,0 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import { Root } from './Root'
-import React from 'react'
-
-const router = createBrowserRouter([{ path: '*', element: Root() }])
-
-export default function RouterEntrance() {
-  return <RouterProvider router={router} />
-}
